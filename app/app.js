5332bc20c709f23357d1bfc7940f8b272857594a
jv-jv <jacopo.v@pm.me>
2022-11-05 14:43:10 +0000

    connect mongo

--- app/app.js
+++ app/app.js
@@ -2,9 +2,22 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const { MongoClient } = require("mongodb")
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb+srv://testUser:testPassword@cluster0.qtg7mac.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri)
+console.log("client :", client)
+
+// testUser
+// testPassword
+
 app.get("/", (req, res) => {
-  console.log("req", req)
   res.send("Yellowwwww")
+})
+
+app.get("/ciao", (req, res) => {
+  res.send(JSON.stringify(client))
 })
 
 app.listen(port, () => {
